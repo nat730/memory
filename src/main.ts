4307182195d91f96b9ca9888d67e90e542ca8b3f
Zelie-C <zelie.chemin@gmail.com>
2023-08-25 16:03:09 +0200

    nettoyage code

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,7 @@ function shuffleArray(array: any[]) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
-}
-//creer le compteur
+};
 
 let secondes = 0;
 let StartCompteur = false;
@@ -19,23 +18,21 @@ function tictac() {
         const secondesAffichage = (secondes % 60).toString().padStart(2, '0');
         chronoContainer.innerText = `temps écoulé : ${minutesAffichage}:${secondesAffichage}`;
     }
-}
+};
 
 
-// Créer un tableau de valeurs de couleurs
 const tileColors = ['black', 'white', 'green', 'yellow', 'purple', 'orange', 'pink', 'cyan', 'black', 'white', 'green', 'yellow', 'purple', 'orange', 'pink', 'cyan'];
 shuffleArray(tileColors);
 
-// Créer un conteneur pour le plateau de jeu
 const gameContainer = document.querySelector('#game-container') as HTMLDivElement;
 const menuContainer = document.querySelector("#menu-container") as HTMLDivElement;
 const restartButton = document.createElement("button") as HTMLButtonElement ;
 const compteurContainer = document.querySelector('#compteur-container') as HTMLDivElement;
 
 
-let flippedTiles: HTMLElement[] = []; // tableau pour récupérer les tuiles retournées
-let wonTiles: string[] = [] // tableau pour récupérer le nombre de tuiles gagnés
-let compteur = 0 
+let flippedTiles: HTMLElement[] = [];
+let wonTiles: string[] = [];
+let compteur = 0;
 function clic(tileElement: HTMLElement) {
     if (flippedTiles.length < 2 && !flippedTiles.includes(tileElement) && tileElement.style.backgroundColor !== 'red') {
         tileElement.style.backgroundColor = tileElement.getAttribute('data-tile-id')!;
@@ -44,7 +41,6 @@ function clic(tileElement: HTMLElement) {
             const tile1 = flippedTiles[0].getAttribute('data-tile-id');
             const tile2 = flippedTiles[1].getAttribute('data-tile-id');
 
-            // Les tuiles correspondent
             if (tile1 === tile2) {
                 flippedTiles.forEach(tile => {
                     tile.style.backgroundColor = 'red';
@@ -52,16 +48,13 @@ function clic(tileElement: HTMLElement) {
                 flippedTiles = [];
                 wonTiles.push("X");
 
-            } if (wonTiles.length == 8) { // Afficher bouton restart quand partie terminée
+            } if (wonTiles.length == 8) {
                 restartButton.innerText = "Recommencer";
                 restartButton.setAttribute("id", "restartbutton");
                 menuContainer.appendChild(restartButton);
                 restartButton.style.display = "block";
                 gameContainer.classList.add("less-opacity")
-            }
-                
-            else {
-                // Les tuiles ne correspondent pas
+            } else {
                 setTimeout(() => {
                     flippedTiles.forEach(tile => {
                         tile.style.backgroundColor = 'blue';
@@ -79,46 +72,42 @@ function clic(tileElement: HTMLElement) {
         } else {
             compteurContainer.innerText = "";
             compteurContainer.classList.add("hidden");
-        }
-        
+        }  
     }
     compteurContainer.innerText = `nombre de coups : ${compteur}`;
 }
     compteurContainer.innerText = "";
     compteurContainer.classList.add("hidden");
 
-// Créer et afficher les tuiles sur le plateau de jeu
 function init(){
-
-tileColors.forEach((color) => {
-    const tileElement = document.createElement('div');
-    tileElement.classList.add('tile');
-    tileElement.style.backgroundColor = 'blue';
-    tileElement.setAttribute('data-tile-id', color);
-    tileElement.addEventListener('click', () => {
-        clic(tileElement);
-    });
+    tileColors.forEach((color) => {
+        const tileElement = document.createElement('div');
+        tileElement.classList.add('tile');
+        tileElement.style.backgroundColor = 'blue';
+        tileElement.setAttribute('data-tile-id', color);
+        tileElement.addEventListener('click', () => {
+            clic(tileElement);
+        });
     gameContainer?.appendChild(tileElement);
 });
 }
 
-// Afficher le plateau de jeu avec un bouton de démarrage
 let startButton = document.querySelector("#startbutton") as HTMLButtonElement;
 let dogstartButton = document.querySelector("#dog-start-button") as HTMLButtonElement;
 
 function buttonRemover() {
     startButton.remove();
     dogstartButton.remove();
-}
+};
 
 startButton.addEventListener("click", () => {
-    buttonRemover()
+    buttonRemover();
     gameContainer?.classList.remove("hidden-visibility");
     chronoContainer?.classList.remove("hidden-visibility");
     compteurContainer?.classList.remove("hidden-visibility");
     init();
-    StartCompteur = true;  // Démarrage du compteur
-    chronoInterval = setInterval(tictac, 1000); // Démarrage du chronomètre
+    StartCompteur = true;
+    chronoInterval = setInterval(tictac, 1000);
 })
 
 dogstartButton.addEventListener("click", () => {
@@ -126,12 +115,11 @@ dogstartButton.addEventListener("click", () => {
     gameContainer?.classList.remove("hidden-visibility");
     chronoContainer?.classList.remove("hidden-visibility");
     dogInit();
-    StartCompteur = true;  // Démarrage du compteur
-    chronoInterval = setInterval(tictac, 1000); // Démarrage du chronomètre
+    StartCompteur = true;
+    chronoInterval = setInterval(tictac, 1000);
 });
 
 
-// Relance la partie quand le bouton est cliqué
 restartButton.addEventListener("click", () => {
     wonTiles = [];
     restartButton.style.display = "none";
@@ -142,20 +130,17 @@ restartButton.addEventListener("click", () => {
     dogInit();
 })
 
-// Partie avec les images de chien
 let dogsDatas: string[] = []
 let dogFirstFlippedTile: HTMLElement | null;
 let dogWonTiles: HTMLElement[] = [];
 
 async function getDogs() {
     try {
-
         for(let i = 0; i < 8; i++){
             const reponse = await fetch("https://dog.ceo/api/breeds/image/random")
             const dogData = await reponse.json();
             dogsDatas.push(dogData.message)
         }
-        console.log('reponse', dogsDatas)
     }
     catch(err){
         console.error('error', err)
@@ -171,22 +156,18 @@ function dogInit() {
     dogTile.classList.add("tile");
     dogTile.addEventListener("click", () => {
         dogTile.setAttribute("src", dogsDatas[Math.floor(i/2)]);
-            if (dogFirstFlippedTile === null) {
-                dogFirstFlippedTile = dogTile;
-                console.log(dogFirstFlippedTile);
-            }
-            else if (dogFirstFlippedTile?.getAttribute("src") === dogTile.getAttribute("src")) {
-                console.log(dogTile.getAttribute("src"))
-                dogWonTiles.push(dogFirstFlippedTile);
+        if (dogFirstFlippedTile === null) {
+            dogFirstFlippedTile = dogTile;
+            console.log(dogFirstFlippedTile);
+        } else if (dogFirstFlippedTile?.getAttribute("src") === dogTile.getAttribute("src")) {
+            console.log(dogTile.getAttribute("src"))
+            dogWonTiles.push(dogFirstFlippedTile);
               if (dogWonTiles.length == 8){
                 gameContainer.classList.add("less-opacity");
-              }
-              else {
+              } else {
                 dogFirstFlippedTile = null;
-                compteur++;
               }
-            }
-            else {
+            } else {
               setTimeout(() => {
                 dogFirstFlippedTile?.removeAttribute("src");
                 dogTile.removeAttribute("src");
@@ -195,7 +176,7 @@ function dogInit() {
               compteur++;
             }
           });
-          return dogTile;
+        return dogTile;
     })
     dogTilesElement.sort( () => Math.random() - 0.5);
     dogTilesElement.forEach( dogTile => gameContainer.appendChild(dogTile));
