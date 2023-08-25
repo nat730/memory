d6c63dac15ac0d7d5d3cf8e4d99422af091ba6a9
nat730 <natanaelcolart@gmail.com>
2023-08-25 11:43:08 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -8,18 +8,22 @@ function shuffleArray(array: any[]) {
 //creer le compteur
 
 let compteur = 0;
-  let secondes = 0;
+let secondes = 0;
+let StartCompteur = false;
+let chronoInterval;
 
 
   let ChronoContainer = document.querySelector("#chrono-container") as HTMLDivElement;
   let chrono = window.setInterval(tictac, 1000);    
 
   function tictac() {
+    if (StartCompteur) {
         secondes++;
         const minutesAffichage = Math.floor(secondes / 60);
         const secondesAffichage = (secondes % 60).toString().padStart(2, '0');
         ChronoContainer.innerText = `temps écoulé : ${minutesAffichage}:${secondesAffichage}`;
     }
+}
     
 
 
@@ -29,10 +33,13 @@ const tileColors = ['black', 'white', 'green', 'yellow', 'purple', 'orange', 'pi
 shuffleArray(tileColors);
 
 // Créer un conteneur pour le plateau de jeu
-const gameContainer = document.querySelector('#game-container') ;
+const gameContainer = document.querySelector('#game-container') as HTMLDivElement;
+const menuContainer = document.querySelector("#menu-container") as HTMLDivElement;
+const restartButton = document.createElement("button") as HTMLButtonElement ;
 const compteurContainer = document.querySelector('#compteur-container') as HTMLDivElement;
 
 let flippedTiles: HTMLElement[] = []; // tableau pour récupérer les tuiles retournées
+let wonTiles: string[] = [] // tableau pour récupérer le nombre de tuiles gagnés
 
 // Fonction pour gérer le clic sur une tuile
 function clic(tileElement: HTMLElement) {
@@ -42,15 +49,23 @@ function clic(tileElement: HTMLElement) {
         if (flippedTiles.length === 2) {
             const tile1 = flippedTiles[0].getAttribute('data-tile-id');
             const tile2 = flippedTiles[1].getAttribute('data-tile-id');
-            
+
             // Les tuiles correspondent
             if (tile1 === tile2) {
                 flippedTiles.forEach(tile => {
                     tile.style.backgroundColor = 'red';
                 });
                 flippedTiles = [];
-                compteur++;
-            } 
+                wonTiles.push("X");
+
+            } if (wonTiles.length == 8) { // Afficher bouton restart quand partie terminée
+                restartButton.innerText = "Recommencer";
+                restartButton.setAttribute("id", "restartbutton");
+                menuContainer.appendChild(restartButton);
+                restartButton.style.display = "block";
+                gameContainer.classList.add("less-opacity")
+            }
+                
             else {
                 // Les tuiles ne correspondent pas
                 setTimeout(() => {
@@ -59,15 +74,26 @@ function clic(tileElement: HTMLElement) {
                         tile.innerText = "";
                     });
                     flippedTiles = [];
-                }, 300);
+                }, 500);
                 compteur++;
             }
         }
+        
+        if (compteur > 0) {
+            compteurContainer.innerText = `nombre de coups : ${compteur}`;
+            compteurContainer.classList.remove("hidden");
+        } else {
+            compteurContainer.innerText = "";
+            compteurContainer.classList.add("hidden");
+        }
+        
     }
-    compteurContainer.innerText = `nombre de coups : ${compteur}`;
 }
+    compteurContainer.innerText = "";
+    compteurContainer.classList.add("hidden");
 
 // Créer et afficher les tuiles sur le plateau de jeu
+function init(){
 
 tileColors.forEach((color) => {
     const tileElement = document.createElement('div');
@@ -78,4 +104,34 @@ tileColors.forEach((color) => {
         clic(tileElement);
     });
     gameContainer?.appendChild(tileElement);
+});
+}
+init();
+// Afficher le plateau de jeu avec un bouton de démarrage
+let startButton = document.querySelector("#startbutton") as HTMLButtonElement;
+
+startButton.addEventListener("click", () => {
+    startButton.remove();
+    gameContainer?.classList.remove("hidden-visibility");
+    StartCompteur = true; // Démarrer le compteur
+    secondes = 0;
+});
+
+
+
+
+// Relance la partie quand le bouton est cliqué
+restartButton.addEventListener("click", () => {
+    wonTiles = [];
+    secondes = 0
+    chrono = 0
+    compteur = 0;
+    compteurContainer.innerText = "";
+    compteurContainer.classList.add("hidden");
+    restartButton.style.display = "none";
+    gameContainer.classList.remove("less-opacity");
+    const findTiles = document.querySelectorAll(".tile");
+    findTiles?.forEach ( tile => tile.remove());
+    shuffleArray(tileColors);   
+    init();
 })
